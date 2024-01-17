b6e92a6edda4b263590dafe448e2c581712c6d19
lucasmarinn <lucasmarin27@gmail.com>
2024-01-16 21:10:45 -0300

    codigo feito

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("a")
+class classesJogo{
+    constructor(nome, idade, tipo){
+        this.nome = nome
+        this.idade = idade
+        this.tipo = tipo
+    }
+    atacar(ataque){
+        if (this.tipo === "mago"){
+            this.ataque = "magia"
+        } else if (this.tipo === "guerreiro"){
+            this.ataque = "espada"
+        } else if (this.tipo === "mmonge"){
+            this.ataque = "artes marciais"
+        } else {
+            this.ataque = "shuriken"
+        }
+        console.log(`O tipo ${this.tipo} atacou usando ${this.ataque}`)
+    }
+}
+
+let Jogador1 = new classesJogo("Luke", 19, "mago")
+Jogador1.atacar()
